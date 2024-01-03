f2d370ac9c851f91c808b3e4ef5c42d1d594544d
TenzinV <84941630+TenzinV@users.noreply.github.com>
2024-01-03 14:10:05 -0500

    Add files via upload

---
+++ Program2/Program2/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace Program2
+{
+    partial class deliveryCost
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.peopleLabel = new System.Windows.Forms.Label();
+            this.distanceLabel = new System.Windows.Forms.Label();
+            this.deliveryTitle = new System.Windows.Forms.Label();
+            this.companyATitle = new System.Windows.Forms.Label();
+            this.companyBTitle = new System.Windows.Forms.Label();
+            this.CompanyCTitle = new System.Windows.Forms.Label();
+            this.compAOut = new System.Windows.Forms.Label();
+            this.compBOut = new System.Windows.Forms.Label();
+            this.compCOut = new System.Windows.Forms.Label();
+            this.resultOut = new System.Windows.Forms.Label();
+            this.calculate = new System.Windows.Forms.Button();
+            this.peopleInput = new System.Windows.Forms.TextBox();
+            this.deliveryInput = new System.Windows.Forms.TextBox();
+            this.distanceInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Location = new System.Drawing.Point(355, 43);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(63, 20);
+            this.title.TabIndex = 0;
+            this.title.Text = "Results";
+            // 
+            // peopleLabel
+            // 
+            this.peopleLabel.AutoSize = true;
+            this.peopleLabel.Location = new System.Drawing.Point(190, 101);
+            this.peopleLabel.Name = "peopleLabel";
+            this.peopleLabel.Size = new System.Drawing.Size(62, 20);
+            this.peopleLabel.TabIndex = 1;
+            this.peopleLabel.Text = "People:";
+            // 
+            // distanceLabel
+            // 
+            this.distanceLabel.AutoSize = true;
+            this.distanceLabel.Location = new System.Drawing.Point(190, 151);
+            this.distanceLabel.Name = "distanceLabel";
+            this.distanceLabel.Size = new System.Drawing.Size(126, 20);
+            this.distanceLabel.TabIndex = 2;
+            this.distanceLabel.Text = "Distance (Miles):";
+            // 
+            // deliveryTitle
+            // 
+            this.deliveryTitle.AutoSize = true;
+            this.deliveryTitle.Location = new System.Drawing.Point(190, 191);
+            this.deliveryTitle.Name = "deliveryTitle";
+            this.deliveryTitle.Size = new System.Drawing.Size(112, 20);
+            this.deliveryTitle.TabIndex = 3;
+            this.deliveryTitle.Text = "Delivery Days: ";
+            // 
+            // companyATitle
+            // 
+            this.companyATitle.AutoSize = true;
+            this.companyATitle.Location = new System.Drawing.Point(488, 113);
+            this.companyATitle.Name = "companyATitle";
+            this.companyATitle.Size = new System.Drawing.Size(132, 20);
+            this.companyATitle.TabIndex = 4;
+            this.companyATitle.Text = "Company A Cost:";
+            // 
+            // companyBTitle
+            // 
+            this.companyBTitle.AutoSize = true;
+            this.companyBTitle.Location = new System.Drawing.Point(488, 152);
+            this.companyBTitle.Name = "companyBTitle";
+            this.companyBTitle.Size = new System.Drawing.Size(132, 20);
+            this.companyBTitle.TabIndex = 5;
+            this.companyBTitle.Text = "Company B Cost:";
+            // 
+            // CompanyCTitle
+            // 
+            this.CompanyCTitle.AutoSize = true;
+            this.CompanyCTitle.Location = new System.Drawing.Point(488, 192);
+            this.CompanyCTitle.Name = "CompanyCTitle";
+            this.CompanyCTitle.Size = new System.Drawing.Size(132, 20);
+            this.CompanyCTitle.TabIndex = 6;
+            this.CompanyCTitle.Text = "Company C Cost:";
+            // 
+            // compAOut
+            // 
+            this.compAOut.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.compAOut.Location = new System.Drawing.Point(631, 112);
+            this.compAOut.Name = "compAOut";
+            this.compAOut.Size = new System.Drawing.Size(100, 23);
+            this.compAOut.TabIndex = 7;
+            // 
+            // compBOut
+            // 
+            this.compBOut.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.compBOut.Location = new System.Drawing.Point(631, 148);
+            this.compBOut.Name = "compBOut";
+            this.compBOut.Size = new System.Drawing.Size(100, 23);
+            this.compBOut.TabIndex = 8;
+            // 
+            // compCOut
+            // 
+            this.compCOut.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.compCOut.Location = new System.Drawing.Point(631, 191);
+            this.compCOut.Name = "compCOut";
+            this.compCOut.Size = new System.Drawing.Size(100, 23);
+            this.compCOut.TabIndex = 9;
+            // 
+            // resultOut
+            // 
+            this.resultOut.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultOut.Location = new System.Drawing.Point(492, 246);
+            this.resultOut.Name = "resultOut";
+            this.resultOut.Size = new System.Drawing.Size(240, 23);
+            this.resultOut.TabIndex = 10;
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(325, 313);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(137, 34);
+            this.calculate.TabIndex = 11;
+            this.calculate.Text = "Calculate Cost";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // peopleInput
+            // 
+            this.peopleInput.Location = new System.Drawing.Point(344, 106);
+            this.peopleInput.Name = "peopleInput";
+            this.peopleInput.Size = new System.Drawing.Size(100, 26);
+            this.peopleInput.TabIndex = 12;
+            // 
+            // deliveryInput
+            // 
+            this.deliveryInput.Location = new System.Drawing.Point(344, 188);
+            this.deliveryInput.Name = "deliveryInput";
+            this.deliveryInput.Size = new System.Drawing.Size(100, 26);
+            this.deliveryInput.TabIndex = 13;
+            // 
+            // distanceInput
+            // 
+            this.distanceInput.Location = new System.Drawing.Point(344, 151);
+            this.distanceInput.Name = "distanceInput";
+            this.distanceInput.Size = new System.Drawing.Size(100, 26);
+            this.distanceInput.TabIndex = 14;
+            // 
+            // deliveryCost
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.distanceInput);
+            this.Controls.Add(this.deliveryInput);
+            this.Controls.Add(this.peopleInput);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.resultOut);
+            this.Controls.Add(this.compCOut);
+            this.Controls.Add(this.compBOut);
+            this.Controls.Add(this.compAOut);
+            this.Controls.Add(this.CompanyCTitle);
+            this.Controls.Add(this.companyBTitle);
+            this.Controls.Add(this.companyATitle);
+            this.Controls.Add(this.deliveryTitle);
+            this.Controls.Add(this.distanceLabel);
+            this.Controls.Add(this.peopleLabel);
+            this.Controls.Add(this.title);
+            this.Name = "deliveryCost";
+            this.Text = "Food Delivery Cost Calculation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label peopleLabel;
+        private System.Windows.Forms.Label distanceLabel;
+        private System.Windows.Forms.Label deliveryTitle;
+        private System.Windows.Forms.Label companyATitle;
+        private System.Windows.Forms.Label companyBTitle;
+        private System.Windows.Forms.Label CompanyCTitle;
+        private System.Windows.Forms.Label compAOut;
+        private System.Windows.Forms.Label compBOut;
+        private System.Windows.Forms.Label compCOut;
+        private System.Windows.Forms.Label resultOut;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.TextBox peopleInput;
+        private System.Windows.Forms.TextBox deliveryInput;
+        private System.Windows.Forms.TextBox distanceInput;
+    }
+}
+
---
+++ Program2/Program2/Form1.cs
@@ -0,0 +1,123 @@
+﻿//Program 2
+//CIS 199-01
+//Due: 3-11-2021
+//By: R2889
+
+//This program prompts the user to enter values and calculates the cost of three companies based on what
+//each company charges. The program will tell the user which company is the cheapest after displaying all
+//three prices.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class deliveryCost : Form
+    {
+        public deliveryCost()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            const int compAPeople = 1; //per person fee company a charges
+            const double compADistance = .02; //distance fee for company a 
+            const double compBDistance = .10; //distance fee for company b
+            const double compCPeople = .25; //per person fee for company c
+            const int compCDelivery = 20; //delivery fee for company c
+
+            int people; //int for the number of people the user entered 
+            double distance; //double for the distance the user entered
+            int delivery; //int for the number of days the user entered
+
+            double compA; //will be used for total cost of company a
+            double compB; //will be used for total cost of company b
+            double compC; //will be used for total cost of company c
+
+            int compADays = 0; //int to help calculate delivery fee for company a
+            double compBPeople = 0; //double to help calculate person fee for company b
+            double compBDelivery = 0; //double to hlep calculate delivery fee for company b
+            int compCDistance = 0; //int to help calcute additional delivery fee  for company c
+            //tryparses each of the inputs to check whether the user entered valid information
+            if (int.TryParse(peopleInput.Text, out people))
+            {
+                if (double.TryParse(distanceInput.Text, out distance))
+                {
+                    if (int.TryParse(deliveryInput.Text, out delivery))
+                    {
+                        //checks the delivery and inputs the according fees 
+                        if (delivery == 1)
+                            compADays = 20;
+                        if (delivery == 2)
+                            compADays = 17;
+                        if (delivery == 3)
+                            compADays = 15;
+                        if (delivery >= 4 && delivery <= 7)
+                            compADays = 10;
+                        if (delivery > 7)
+                            compADays = 7;
+                        //calculates the total cost of company a 
+                        compA = (people * compAPeople) + (distance * compADistance) + compADays;
+                        //checks the people and delivery and inputs the according fees
+                        if (people < 10)
+                            compBPeople = 20;
+                        if (people >= 10 && people < 50)
+                            compBPeople = 10;
+                        if (people >= 50 && people < 100)
+                            compBPeople = 5;
+                        if (people >= 100 && people < 200)
+                            compBPeople = 3;
+                        if (people >= 200)
+                        {
+                            compBPeople = .15;
+                        }
+                        if (delivery >= 1 && delivery < 4)
+                            compBDelivery = 10;
+                        if (delivery > 4)
+                            compBDelivery = 7;
+                        //calculates the total cost of company b
+                        compB = (people * compBPeople) + (distance * compBDistance) + compBDelivery;
+                        //checks the distance entered and adds the according fee 
+                        if (distance >= 1000)
+                            compCDistance = 40;
+                        if (distance >= 750 && distance < 1000)
+                            compCDistance = 35;                 
+                        if (distance >= 500 && distance < 750)
+                            compCDistance = 25;
+                        if (distance >= 200 && distance < 500)
+                            compCDistance = 15;
+                        if (distance >= 0 && distance < 200)
+                            compCDistance = 10;
+                        //calculates the toal cost of company c
+                        compC = (people * compCPeople) + compCDistance + compCDelivery;
+                        //outputs the total cost of each company
+                        compAOut.Text = $"{compA:c}";
+                        compBOut.Text = $"{compB:c}";
+                        compCOut.Text = $"{compC:c}";
+                        //outputs which company is the cheapest of the three
+                        if (compA < compB && compA < compC)
+                            resultOut.Text = "The lowest company is: A";
+                        if (compB < compA && compB < compC)
+                            resultOut.Text = "The lowest company is: B";
+                        if (compC < compA && compC < compB)
+                            resultOut.Text = "The lowest company is: C";
+                    }
+                    else
+                        MessageBox.Show("Invalid Delivery Days"); //tells the user the delivery days is wrong
+                }
+                else
+                    MessageBox.Show("Invalid Distance"); //tells the user the distance is wrong
+            }
+            else
+                MessageBox.Show("Invalid Number of People"); //tells the user the number of people is wrong
+        }
+    }
+}
